5c85f9fee60bb5911c83e0f995bff3bb45d96dd0
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2023-02-13 15:33:00 +0100

    Kino.Player.Sink accepts separate video and audio
    Separate video and audio channels
    Prepared for mp4 stream

--- lib/membrane/kino/player_sink.ex
+++ lib/membrane/kino/player_sink.ex
@@ -32,8 +32,34 @@ defmodule Membrane.Kino.Player.Sink do
   RC.Pipeline.exec_actions(pipeline, spec: structure)
   RC.Pipeline.exec_actions(pipeline, playback: :playing)
   ```
-
   """
+
+  defmodule Track do
+    @moduledoc false
+
+    defstruct pad: nil, buffered: Qex.new()
+
+    def new() do
+      %__MODULE__{}
+    end
+
+    def set_pad(%__MODULE__{} = track, pad) do
+      %__MODULE__{track | pad: pad}
+    end
+
+    def ready?(%__MODULE__{buffered: buffered}) do
+      not Enum.empty?(buffered)
+    end
+
+    def push(%__MODULE__{buffered: buffered} = track, buffer) do
+      %__MODULE__{track | buffered: Qex.push(buffered, buffer)}
+    end
+
+    def pop!(%__MODULE__{buffered: buffered} = track) do
+      {buffer, buffered} = Qex.pop!(buffered)
+      {buffer, %__MODULE__{track | buffered: buffered}}
+    end
+  end
 
   use Membrane.Sink
 
@@ -48,18 +74,59 @@ defmodule Membrane.Kino.Player.Sink do
                   "Kino element handle. It should be initialized before the pipeline is started."
               ]
 
-  def_input_pad :input,
-    accepted_format: any_of(H264, AAC),
-    demand_unit: :buffers
+  def_input_pad :audio,
+    accepted_format: AAC,
+    demand_unit: :buffers,
+    availability: :on_request
+
+  def_input_pad :video,
+    accepted_format: H264,
+    demand_unit: :buffers,
+    availability: :on_request
 
   @impl true
   def handle_init(_ctx, %__MODULE__{} = options) do
-    state = %{kino: options.kino, timer_started?: false, index: 0, framerate: nil}
+    kino = options.kino
+    type = KinoPlayer.call(kino, :get_type)
+
+    tracks =
+      case type do
+        :video -> %{video: Track.new()}
+        :audio -> %{audio: Track.new()}
+        :both -> %{video: Track.new(), audio: Track.new()}
+      end
+
+    state = %{
+      kino: kino,
+      timer_started?: false,
+      index: 0,
+      framerate: nil,
+      type: type,
+      tracks: tracks
+    }
+
     {[], state}
   end
 
   @impl true
-  def handle_stream_format(:input, stream_format, ctx, state) do
+  def handle_stream_format(
+        {_mod, pad, _ref} = pad_ref,
+        stream_format,
+        _ctx,
+        %{type: type} = state
+      )
+      when pad in [:video, :audio] and type == pad do
+    %{kino: kino} = state
+    {num, den} = get_framerate(stream_format)
+    framerate_float = num / den
+    KinoPlayer.cast(kino, {:create, framerate_float})
+    tracks = %{state.tracks | pad => Track.set_pad(state.tracks[pad], pad_ref)}
+
+    {[], %{state | framerate: {num, den}, tracks: tracks}}
+  end
+
+  @impl true
+  def handle_stream_format(_pad, stream_format, ctx, %{type: :both, framerate: nil} = state) do
     %{input: input} = ctx.pads
     %{kino: kino} = state
 
@@ -73,6 +140,11 @@ defmodule Membrane.Kino.Player.Sink do
     end
   end
 
+  @impl true
+  def handle_stream_format(_pad, _stream_format, _ctx, %{type: :both} = state) do
+    {[], state}
+  end
+
   defp get_framerate(stream_format = %H264{}) do
     stream_format.framerate
   end
@@ -84,28 +156,77 @@ defmodule Membrane.Kino.Player.Sink do
   end
 
   @impl true
-  def handle_start_of_stream(:input, _ctx, state) do
-    {nom, denom} = state.framerate
+  def handle_start_of_stream(pad, _ctx, state) do
+    timer_actions =
+      if state.timer_started? do
+        []
+      else
+        {nom, denom} = state.framerate
+        timer = {:demand_timer, Ratio.new(Time.seconds(denom), nom)}
+        [start_timer: timer]
+      end
 
-    timer = {:demand_timer, Ratio.new(Time.seconds(denom), nom)}
+    demand_actions = [demand: pad]
 
-    {[demand: :input, start_timer: timer], %{state | timer_started?: true}}
+    {timer_actions ++ demand_actions, %{state | timer_started?: true}}
   end
 
   @impl true
-  def handle_write(:input, %Buffer{payload: payload}, _ctx, state) do
-    payload = Membrane.Payload.to_binary(payload)
-    KinoPlayer.cast(state.kino, {:buffer, payload, %{index: state.index}})
-    {[], %{state | index: state.index + 1}}
+  def handle_write({_mod, pad, _ref}, %Buffer{payload: payload}, _ctx, state) do
+    %{tracks: tracks} = state
+
+    tracks = %{tracks | pad => Track.push(tracks[pad], payload)}
+    state = %{state | tracks: tracks}
+
+    if ready_to_send?(tracks) do
+      {buffers, tracks} = pop_buffers(tracks)
+
+      payload = prepare_payload(buffers, state.type)
+
+      KinoPlayer.cast(state.kino, {:buffer, payload, %{index: state.index}})
+
+      {[], %{state | index: state.index + 1, tracks: Map.new(tracks)}}
+    else
+      {[], state}
+    end
+  end
+
+  defp ready_to_send?(tracks) do
+    Map.values(tracks) |> Enum.all?(&Track.ready?/1)
+  end
+
+  defp prepare_payload(buffers, type) do
+    case type do
+      :video ->
+        buffers |> then(fn [{_name, buffer}] -> buffer end) |> Membrane.Payload.to_binary()
+
+      :audio ->
+        buffers |> then(fn [{_name, buffer}] -> buffer end) |> Membrane.Payload.to_binary()
+
+      :both ->
+        Map.new(buffers, fn {name, buffer} -> {name, Membrane.Payload.to_binary(buffer)} end)
+    end
+  end
+
+  defp pop_buffers(tracks) do
+    tracks
+    |> Enum.map(fn {name, track} -> {name, Track.pop!(track)} end)
+    |> Enum.map(fn {name, {buffer, track}} -> {{name, buffer}, {name, track}} end)
+    |> Enum.unzip()
   end
 
   @impl true
   def handle_tick(:demand_timer, _ctx, state) do
-    {[demand: :input], state}
+    demand_actions = get_demand_actions(state.tracks)
+    {demand_actions, state}
+  end
+
+  defp get_demand_actions(tracks) do
+    Map.values(tracks) |> Enum.map(fn %Track{pad: pad} -> {:demand, pad} end)
   end
 
   @impl true
-  def handle_end_of_stream(:input, _ctx, state) do
+  def handle_end_of_stream(_pad, _ctx, state) do
     if state.timer_started? do
       {[stop_timer: :demand_timer], %{state | timer_started?: false, index: 0}}
     else
