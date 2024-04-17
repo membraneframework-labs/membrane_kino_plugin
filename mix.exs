b473cb3631ccb94bf4ac74a9896457fe2c5514a3
Bartek Chaliński <b.chalinski@gmail.com>
2024-04-17 02:55:41 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Kino.Mixfile do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @github_url "https://github.com/membraneframework-labs/membrane_kino_plugin/"
 
   def project do
