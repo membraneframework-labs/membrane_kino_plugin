e3370378d55e5114235ed85885f7c71ee9013db3
Bartek Chaliński <b.chalinski@gmail.com>
2024-05-21 14:54:58 +0200

    html button reacting to kino options

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Membrane.Kino.Mixfile do
       {:membrane_file_plugin, "~> 0.17.0"},
       {:membrane_raw_video_format, "~> 0.4.0", only: :test},
       {:membrane_opus_plugin, "~> 0.19.1", only: :test},
-      {:membrane_aac_fdk_plugin, "~> 0.18.2", only: :test},
+      {:membrane_aac_fdk_plugin, "~> 0.18.8", only: :test},
       {:membrane_matroska_plugin, "~> 0.5.1", only: :test},
       {:membrane_generator_plugin, "~> 0.10.0", only: :test},
 
